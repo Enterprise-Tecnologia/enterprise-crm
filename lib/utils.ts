37992ea04b4a1bd10295ba13b1654a952cc6f1cd
Charles <xbrown@gmail.com>
2024-08-07 09:18:01 -0300

    feat: generate proposal term

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,16 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+
+export const downloadItem = (data: string, fileName: string) => {
+
+  // const url = window.URL.createObjectURL(new Blob(result.data));
+  const link = document.createElement('a');
+  link.href = `data:application/pdf;base64,${data}`;
+  link.setAttribute('download', fileName);
+  document.body.appendChild(link);
+  link.click();
+  link.parentNode?.removeChild(link);
+
+};
